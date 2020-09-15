b4fa374b3685e201dd07414a5fa636b31d571d6f
Ivo Krastev <ivo.krustev@yahoo.com>
2020-09-15 16:33:32 +0300

    building the game

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as actionTypes from "../../store/actions";
 
 const Play = () => {
   const money = useSelector((state) => state.moneyReducer.money);
-  const casinoCards = useSelector((state) => state.scoreReducer.casino);
+  const casinoCards = useSelector((state) => state.scoreReducer.casinoCards);
+  const userCards = useSelector((state) => state.scoreReducer.userCards);
+  const casinoResult = useSelector((state) => state.scoreReducer.casinoResult);
+  const userResult = useSelector((state) => state.scoreReducer.userResult);
   const dispatch = useDispatch();
-  let card = Math.floor(Math.random() * 10) + 2;
-  const [arr, setArr] = useState([]);
 
-  const demo = () => {
+  const hit = () => {
     dispatch({
-      type: actionTypes.INCREASE_MONEY,
+      type: actionTypes.USER_CARD,
+      card: Math.floor(Math.random() * 10) + 2,
     });
-    setArr([...arr, card]);
   };
 
-  const demoTest = () => {
+  const stand = () => {
     dispatch({
       type: actionTypes.CASINO_CARD,
-      card: card,
+      card: Math.floor(Math.random() * 10) + 2,
     });
   };
 
   useEffect(() => {
-    let sum;
-    if (arr.length > 0) {
-      sum = arr.reduce((a, b) => a + b);
-    }
-    console.log(arr);
-    if (sum > 21) {
-      alert("Game over");
-    }
-    console.log(sum);
-  }, [arr]);
+    console.log("heey");
+  }, []);
 
   return (
     <div>
       <h1>{money}</h1>
-      <h2>{card}</h2>
-
-      <button onClick={demo}>+</button>
-      <button onClick={demoTest}>-</button>
+      <button onClick={hit}>HIT</button>
+      <button onClick={stand}>STAND</button>
+      <h1>Casino: {casinoResult < 21 ? casinoResult : "You Win!"}</h1>
       <h2>
         {casinoCards.map((card) => (
+          <p>{card}</p>
+        ))}
+      </h2>
+      <hr />
+      <h1>User: {userResult < 21 ? userResult : "You Lost"}</h1>
+      <h2>
+        {userCards.map((card) => (
           <p>{card}</p>
         ))}
       </h2>
--- src/store/reducers/scoreReducer.js
+++ src/store/reducers/scoreReducer.js
@@ -1,20 +1,34 @@
 import * as actionTypes from "../actions";
 
+const casinoCard1 = Math.floor(Math.random() * 10) + 2;
+const casinoCard2 = Math.floor(Math.random() * 10) + 2;
+const userCard1 = Math.floor(Math.random() * 10) + 2;
+const userCard2 = Math.floor(Math.random() * 10) + 2;
+
 const initialState = {
-  casino: [],
-  user: [],
+  casinoCards: [casinoCard1, casinoCard2],
+  casinoResult: casinoCard1 + casinoCard2,
+  userCards: [userCard1, userCard2],
+  userResult: userCard1 + userCard2,
 };
 
-const score = (state = initialState, action) => {
+const scoreReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.CASINO_CARD:
       return {
         ...state,
-        casino: state.casino.concat(action.card),
+        casinoCards: state.casinoCards.concat(action.card),
+        casinoResult: state.casinoResult + action.card,
+      };
+    case actionTypes.USER_CARD:
+      return {
+        ...state,
+        userCards: state.userCards.concat(action.card),
+        userResult: state.userResult + action.card,
       };
     default:
       return state;
   }
 };
 
-export default score;
+export default scoreReducer;
